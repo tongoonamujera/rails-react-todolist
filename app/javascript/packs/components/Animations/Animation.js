06f7690a3652024da0a672e802e98a53015ff8ed
tongoonamujera <tongoonamujera@gmail.com>
2022-04-29 16:04:10 +0200

    Add custom typewriter hook

--- app/javascript/packs/components/Animations/Animation.js
+++ app/javascript/packs/components/Animations/Animation.js
@@ -1,27 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useAnimation } from '../../CustomHooks/AnimationHook/useAnimtion';
 
 const Animation = ({ text }) => {
   const [typed] = useAnimation(text, 300, 100);
-  // const word = text;
-  // const [typed, setTyped] = useState('');
-  // const [typing, setTyping] = useState(true);
-
-  // useEffect(() => {
-  //   if (typing) {
-  //     setTimeout(() => {
-  //       setTyped(word.slice(0, typed.length + 1))
-  //       if (typed.length === word.length)
-  //         setTyping(false)
-  //     }, 300)
-  //   } else {
-  //     setTimeout(() => {
-  //       setTyped(typed.slice(0, typed.length - 1))
-  //       if (typed.length === 0)
-  //         setTyping(true)
-  //     }, 100)
-  //   }
-  // }, [typed, typing])
 
   return (
     <>
